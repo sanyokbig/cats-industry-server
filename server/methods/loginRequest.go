6b3ded2cb53d3f986e4b18162d49bee8261bc1b8
sanyokbig <sanyokbig@gmail.com>
2018-02-05 12:06:56 +0300

    login state generated now

--- server/methods/loginRequest.go
+++ server/methods/loginRequest.go
@@ -5,6 +5,7 @@ import (
 	"cats-industry-server/schema"
 	"fmt"
 	"github.com/go-errors/errors"
+	"github.com/satori/go.uuid"
 	"log"
 )
 
@@ -16,6 +17,7 @@ var scopeSets = map[string]string{
 var (
 	ErrPayloadParseFailed = errors.New("failed to parse payload")
 	ErrNoScopeSet         = errors.New("scope set not found")
+	ErrStateGenFailed     = errors.New("failed to generate state")
 )
 
 //easyjson:json
@@ -26,18 +28,29 @@ type loginRequestPayload struct {
 func loginRequest(c Client, m schema.Message) (resp *schema.Message, err error) {
 	log.Println("log request from", c.GetID())
 
+	// Parse incoming payload
 	payload := loginRequestPayload{}
 	if err := m.Payload.Deliver(&payload); err != nil {
 		log.Println(err)
 		return nil, ErrPayloadParseFailed
 	}
+
+	// Select scope set for authorization
 	scopes, ok := scopeSets[payload.ScopeSet]
 	if !ok {
 		log.Println(err)
 		return nil, ErrNoScopeSet
 	}
 
-	uri := fmt.Sprintf("https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=%v&client_id=%v&scope=%v&state=%v", config.EveConfig.RedirectUri, config.EveConfig.ClientId, scopes, "so-state-very-unique")
+	// Generate state to identify login response later
+	state, err := uuid.NewV4()
+	if err != nil {
+		log.Println(err)
+		return nil, ErrStateGenFailed
+	}
+
+	// Generate login uri for client
+	uri := fmt.Sprintf("https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=%v&client_id=%v&scope=%v&state=%v", config.EveConfig.RedirectUri, config.EveConfig.ClientId, scopes, state)
 
 	return &schema.Message{
 		Type:    "login_uri",
